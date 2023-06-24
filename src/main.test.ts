70921a17bb501a51b952c7fe7d6022030f24571e
Yota Toyama <raviqqe@gmail.com>
2023-06-24 17:29:41 +1000

    Fix

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { buildPrompt, parseOutputStream } from "./main.js";
+import { toArray } from "@raviqqe/hidash/promise";
 
 describe(buildPrompt.name, () => {
   it("builds a prompt", () => {
@@ -23,20 +24,54 @@ describe(buildPrompt.name, () => {
 });
 
 describe(parseOutputStream.name, () => {
-  it("parses an empty stream", () => {
-    expect(parseOutputStream((async function* () {})())).toMatchSnapshot();
+  it("parses an empty stream", async () => {
+    expect(await toArray(parseOutputStream((async function* () {})()))).toEqual(
+      []
+    );
   });
 
-  it("builds a prompt with two examples", () => {
+  it("parses a word", async () => {
     expect(
-      buildPrompt(
-        "foo",
-        [
-          { input: "bar", output: "baz" },
-          { input: "answer", output: "42" },
-        ],
-        "qux"
+      await toArray(
+        parseOutputStream(
+          (async function* () {
+            yield "foo";
+          })()
+        )
       )
-    ).toMatchSnapshot();
+    ).toEqual(["foo"]);
+  });
+
+  it("parses a wrapped word", async () => {
+    expect(
+      await toArray(
+        parseOutputStream(
+          (async function* () {
+            yield '"""';
+            yield "\n";
+            yield "foo";
+            yield '"""';
+            yield "\n";
+          })()
+        )
+      )
+    ).toEqual(["foo"]);
+  });
+
+  it("parses wrapped words", async () => {
+    expect(
+      await toArray(
+        parseOutputStream(
+          (async function* () {
+            yield '"""';
+            yield "\n";
+            yield "foo";
+            yield "bar";
+            yield '"""';
+            yield "\n";
+          })()
+        )
+      )
+    ).toEqual(["foo", "bar"]);
   });
 });
