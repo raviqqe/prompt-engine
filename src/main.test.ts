1b34a2c2769ae8988ea50bae71fba0aa7f129c78
Yota Toyama <raviqqe@gmail.com>
2023-06-24 17:21:06 +1000

    Fix

--- src/main.test.ts
+++ src/main.test.ts
@@ -7,4 +7,17 @@ describe(buildPrompt.name, () => {
       buildPrompt("foo", [{ input: "bar", output: "baz" }], "qux")
     ).toMatchSnapshot();
   });
+
+  it("builds a prompt with two examples", () => {
+    expect(
+      buildPrompt(
+        "foo",
+        [
+          { input: "bar", output: "baz" },
+          { input: "answer", output: "42" },
+        ],
+        "qux"
+      )
+    ).toMatchSnapshot();
+  });
 });
