06e88db63906fc83cdf696a4f80968a0c906575b
Yota Toyama <raviqqe@gmail.com>
2023-06-24 17:22:23 +1000

    fix

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,11 +1,30 @@
 import { describe, expect, it } from "vitest";
-import { buildPrompt } from "./main.js";
+import { buildPrompt, parseOutputStream } from "./main.js";
 
 describe(buildPrompt.name, () => {
   it("builds a prompt", () => {
     expect(
       buildPrompt("foo", [{ input: "bar", output: "baz" }], "qux")
     ).toMatchSnapshot();
+  });
+
+  it("builds a prompt with two examples", () => {
+    expect(
+      buildPrompt(
+        "foo",
+        [
+          { input: "bar", output: "baz" },
+          { input: "answer", output: "42" },
+        ],
+        "qux"
+      )
+    ).toMatchSnapshot();
+  });
+});
+
+describe(parseOutputStream.name, () => {
+  it("parses an empty stream", () => {
+    expect(parseOutputStream((async function* () {})())).toMatchSnapshot();
   });
 
   it("builds a prompt with two examples", () => {
